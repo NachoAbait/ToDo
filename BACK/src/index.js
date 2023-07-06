6418760971762884039835354cb3a996ebab037f
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-07-06 19:53:42 -0300

    s

--- BACK/src/index.js
+++ BACK/src/index.js
@@ -1,41 +1,32 @@
 import express from "express";
-import authRoutes from "./Routes/authRoutes.js";
-import taskRoutes from "./Routes/taskRoutes.js";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import authRoutes from "./Routes/authRoutes.js";
+import taskRoutes from "./Routes/taskRoutes.js";
+
 dotenv.config();
 const PORT = process.env.PORT || 3001;
-const HOST = process.env.HOST;
 
-// SERVIDOR
 const app = express();
-app.listen(3001, () => {
-  console.log(`Server on Port: ${PORT}`);
-});
 
-//MIDDLEWARES
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://to-do-roan-pi.vercel.app"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+app.use(cors({
+  origin: "https://to-do-roan-pi.vercel.app",
+  credentials: true,
+}));
 
-//RUTAS
 app.use(authRoutes);
 app.use(taskRoutes);
 
-//DATABASE
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
+
 import "./db.js";
