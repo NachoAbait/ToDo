2683d09a33bb06ba264affc42abeabbeadf1b931
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-07-20 15:05:59 -0300

    hola

--- BACK/src/index.js
+++ BACK/src/index.js
@@ -33,12 +33,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
-app.use(
-  cors({
-    origin: "https://to-do-roan-pi.vercel.app",
-    credentials: true,
-  })
-);
+
 
 //RUTAS
 app.use(authRoutes);
