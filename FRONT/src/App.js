7710f342c916b8b8bb1dcd351a0cfc50fce90f32
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-07-20 15:08:01 -0300

    hola

--- FRONT/src/App.js
+++ FRONT/src/App.js
@@ -4,7 +4,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./COMPONENTS/Home/Home.js";
 import Profile from "./COMPONENTS/Profile/Profile.js";
 
+import axios from "axios";
 
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "https://todo-production-a086.up.railway.app/";
 
 function App() {
   return (
