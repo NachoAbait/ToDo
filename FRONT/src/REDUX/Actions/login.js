d0d24a6abaa233f85f0dd8b1ddce9b03bb72cfbf
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-07-20 14:43:11 -0300

    hola

--- FRONT/src/REDUX/Actions/login.js
+++ FRONT/src/REDUX/Actions/login.js
@@ -4,7 +4,7 @@ import { closeModal } from "./closeModal.js";
 const login = (userData) => {
   return (dispatch) => {
     return axios
-      .post("/login", userData)
+      .post("/login", { credentials: true, userData })
       .then((response) => {
         // Usuario creado exitosamente
         // Aquí puedes mostrar una alerta o realizar otras acciones
