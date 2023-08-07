bffd799b68a3b6e6dbaac5f68600ff18ae1f1b96
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2023-08-07 16:28:16 -0300

    cambie el cors

--- BACK/src/index.js
+++ BACK/src/index.js
@@ -23,23 +23,13 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://to-do-roan-pi.vercel.app"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization" // Allow 'Authorization' header
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+const corsOptions = {
+  origin: 'https://to-do-roan-pi.vercel.app',
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
 
-  // Preflight request. Reply successfully:
-  if (req.method === 'OPTIONS') {
-    res.status(200).send();
-    return;
-  }
-
-  next();
-});
+app.use(cors(corsOptions));
 
 //RUTAS
 app.use(authRoutes);
